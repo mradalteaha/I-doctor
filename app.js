8c28391ffaff92f6d183c8f805d6844ea5cc579c
moradte <moradte@ac.sce.ac.il>
2022-04-12 08:01:45 +0300

    testing

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app=express();
 app.use(express.static('views'));
+app.use(bodyParser.urlencoded({extended:true}));
+
 
 app.set("view engine","ejs");
 app.get('/',function(req,res){
     res.render('Home',{style:'Home.css'});
 });
+app.post('/Sign-Up.html',function(req,res){
+    console.log(req.body);
+});
+
+
 app.listen(3000,function(){
     console.log("Starting Server");
 });
