e3184041a3679c519c5a8b9673c7fec3707ffe06
moradte <moradte@ac.sce.ac.il>
2022-04-13 10:20:12 +0300

    updating the node and npm libraries and initializing the data base

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 //jshint esversion:6
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const {TextDecoder, TextEncoder} = require("util");
+database.connect('mongodb://127.0.0.1:27017/usersDB',{useNewUrlParser: true});
+
+
+
+
+const regSchema = new mongoose.Schema({ role: String,
+Name: [ String, String ],
+id: Number,
+password: String,
+pasword: String,
+email: String});
+
+const User = mongoose.model("User",regSchema);
+
 
 const app=express();
 app.use(express.static('views'));
