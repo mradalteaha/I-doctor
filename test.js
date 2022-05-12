9fdee564e50b658b06cc2cd182bc92c499abf73d
Abed Jamal <Abedgm@ac.sce.ac.il>
2022-05-12 03:48:03 +0300

    fixed my branch

--- test.js
+++ test.js
@@ -4,11 +4,46 @@ var app = require('./app.js');
 
 
 
-describe('Good Home Routes', function () {
+describe('Good Home Routes to login page', function () {
 
     test('responds to /', async () => {
-      const res = await request(app).get('/Log-in');
+      const res = await request(app).get('/Log-In');
       expect(res.header['content-type']).toBe('text/html; charset=utf-8');
       expect(res.statusCode).toBe(200);
+    });
+
+});
+
+describe('Good Home Routes to home page', function () {
+    test('responds to /', async () => {
+        const res = await request(app).get('/Home');
+        expect(res.header['content-type']).toBe('text/html; charset=utf-8');
+        expect(res.statusCode).toBe(200);
+    });
+});
+
+describe('Good Home Routes to Register page', function () {
+    test('responds to /', async () => {
+        const res = await request(app).get('/Sign-Up');
+        expect(res.header['content-type']).toBe('text/html; charset=utf-8');
+        expect(res.statusCode).toBe(200);
+    });
+});
+
+
+describe('Good Home Routes to Examinator page', function () {
+    test('responds to /', async () => {
+        const res = await request(app).get('/Examinator');
+        expect(res.header['content-type']).toBe('text/html; charset=utf-8');
+        expect(res.statusCode).toBe(500);
+    });
+});
+
+
+describe('Good Home Routes to Forgot password', function () {
+    test('responds to /', async () => {
+        const res = await request(app).get('/ForgotPW');
+        expect(res.header['content-type']).toBe('text/html; charset=utf-8');
+        expect(res.statusCode).toBe(200);
     });
 });
