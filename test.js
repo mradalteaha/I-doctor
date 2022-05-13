986950e118457a95c78aacbe0ef49befbe1a4bfc
Abed Jamal <Abedgm@ac.sce.ac.il>
2022-05-13 16:02:50 +0300

    Added tests to test.js

--- test.js
+++ test.js
@@ -30,6 +30,33 @@ describe('Check if the routes goes to Home page!', function () {
       expect(res.statusCode).toBe(200);
     });
 });
+describe('Check if the routes goes to Sign Up As Patient!', function () {
+
+  test('responds to /', async () => {
+    const res = await request(app).get('/Sign-Up_As_Patient');
+    expect(res.header['content-type']).toBe('text/html; charset=utf-8');
+    expect(res.statusCode).toBe(404);
+  });
+});
+
+describe('Check if the routes goes to Sign Up As Examinator!', function () {
+
+  test('responds to /', async () => {
+    const res = await request(app).get('/Sign-Up_As_Examinator');
+    expect(res.header['content-type']).toBe('text/html; charset=utf-8');
+    expect(res.statusCode).toBe(404);
+  });
+});
+
+describe('Check if the routes goes to Sign Up As Doctor!', function () {
+
+  test('responds to /', async () => {
+    const res = await request(app).get('/Sign-Up_As_Doctor');
+    expect(res.header['content-type']).toBe('text/html; charset=utf-8');
+    expect(res.statusCode).toBe(404);
+  });
+});
+
 
 // describe('Check if the routes goes to forgot password page!', function () {
 
