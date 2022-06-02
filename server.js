68789d4b25ac82685b7d5aa30b4659c80e3828cd
Abed Jamal <abedjm99@gmail.com>
2022-06-02 21:15:51 +0300

    deploy

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const app = require("./app.js");
 const db = require ('./db/database.js');
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
-db.connect();
 
 const server = app.listen(port,()=>{
     console.log("Starting Server port 3000");
+    db.connect();
 });
 module.exports = server;
